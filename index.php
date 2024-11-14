e52230b3a75476949dfb9ca6eb88b5489edac1e7
Samir Souidi <samirswidi@gmail.com>
2024-11-14 13:54:48 +0100

    changes and update

--- index.php
+++ index.php
@@ -52,7 +52,9 @@
     </style>
 </head>
 <body>
-    <h1>Real-Time Barcode Scanner:deploy in azure 14-11-2024</h1>
+    <h1>Real-Time Barcode Scanner:</h1>
+    <h2>Réalisé par Souidi Samir</h2>
+    <p>deploy in azure 14-11-2024</p>
     <select id="camera-select"></select>
     <video id="video" autoplay></video>
     <div id="scanned-result">Scanned Barcode Number: <span id="result"></span></div>
